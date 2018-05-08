c9b2230bcd727af8e0bf7f2b96416973cccaebe3
teasacura <teasacuraster@gmail.com>
2018-05-08 13:21:37 -0400

    Done.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,6 +37,7 @@ describe User do
       end
 
       it 'has many reservations through their listing' do
+# binding.pry
         expect(katie.reservations).to include(reservation)
       end
     end
