34da7c21984d62695b3dcf868682dfe9ba13fb67
github username <github email address>
2018-05-08 14:48:23 -0400

    done

---
+++ db/migrate/005_create_reviews.rb
@@ -0,0 +1,13 @@
+class CreateReviews < ActiveRecord::Migration
+
+  def change
+    create_table :reviews do |t|
+      t.string :description
+      t.integer :rating
+      t.integer :guest_id
+      t.integer :reservation_id
+      t.integer :listing_id
+    end
+  end
+
+end
